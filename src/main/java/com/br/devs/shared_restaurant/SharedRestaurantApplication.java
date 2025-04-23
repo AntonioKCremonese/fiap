5808db586657523e4aa8e32b40f3651583db7464
Antonio K Cremonese <ak.cremonese@gmail.com>
2025-04-23 18:51:21 -0300

    Criação projeto

---
+++ src/main/java/com/br/devs/shared_restaurant/SharedRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.br.devs.shared_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/devs/shared_restaurant/SharedRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.devs.shared_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
